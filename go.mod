54d220f2b98a4cc5d56e6cf9ff9b2a072b480a99
mytja <mytja@protonmail.com>
2022-04-07 20:17:19 +0200

    Improve URL forwarding

--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module HarmonoidProxy
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/imroc/req/v3 v3.10.0
+)
 
 require (
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/imroc/req/v3 v3.10.0 // indirect
 	golang.org/x/net v0.0.0-20220111093109-d55c255bac03 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/imroc/req/v3"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -22,7 +23,7 @@ func main() {
 	fmt.Println("Starting Harmonoid proxy...")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/music/youtubei/v1/{endpoint}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/{type}/youtubei/v1/{endpoint}", func(w http.ResponseWriter, r *http.Request) {
 		client := req.C()
 		client.DisableAutoReadResponse()
 		request := client.R()
@@ -35,61 +36,77 @@ func main() {
 			}
 		}
 
-		url := fmt.Sprintf("https://music.youtube.com/youtubei/v1/%s", mux.Vars(r)["endpoint"])
+		var uri = ""
+
+		if mux.Vars(r)["type"] == "music" {
+			uri = fmt.Sprintf("https://music.youtube.com/youtubei/v1/%s", mux.Vars(r)["endpoint"])
+		} else if mux.Vars(r)["type"] == "youtube" {
+			uri = fmt.Sprintf("https://www.youtube.com/youtubei/v1/%s", mux.Vars(r)["endpoint"])
+		}
+
+		if uri == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Invalid request"))
+			return
+		}
+
+		var response *req.Response
+		var err error
+
+		parse, err := url.Parse(uri)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Could not parse URI"))
+			return
+		}
+
+		q := parse.Query()
+
+		for name, values := range r.URL.Query() {
+			for _, value := range values {
+				q.Add(name, value)
+			}
+		}
+
+		parse.RawQuery = q.Encode()
+
+		uri = parse.String()
 
 		if r.Method == "GET" {
-			fmt.Printf("Serving GET request to %s using headers %s\n", url, request.Headers)
+			fmt.Printf("Serving GET request to %s using headers %s\n", uri, request.Headers)
 
-			get, err := request.Get(url)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				return
-			}
-			res, err := gzip.NewReader(get.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				w.Write([]byte(err.Error()))
-				return
-			}
-			r, err := io.ReadAll(res)
-			if err != nil {
-				fmt.Println(err.Error())
-				w.Write([]byte(err.Error()))
-				return
-			}
-			w.WriteHeader(get.StatusCode)
-			w.Write(r)
+			response, err = request.Get(uri)
 		} else if r.Method == "POST" {
-			b, err := io.ReadAll(r.Body)
+			var b []byte
+			b, err = io.ReadAll(r.Body)
 			if err != nil {
 				w.Write([]byte(err.Error()))
 				return
 			}
 			request.SetBody(string(b))
 
-			fmt.Printf("Serving POST request to %s using headers %s and body %s\n", url, request.Headers, string(b))
+			fmt.Printf("Serving POST request to %s using headers %s and body %s\n", uri, request.Headers, string(b))
 
-			post, err := request.Post(url)
-			if err != nil {
-				fmt.Println(err.Error())
-				w.Write([]byte(err.Error()))
-				return
-			}
-			res, err := gzip.NewReader(post.Body)
-			if err != nil {
-				fmt.Println(err.Error())
-				w.Write([]byte(err.Error()))
-				return
-			}
-			r, err := io.ReadAll(res)
-			if err != nil {
-				fmt.Println(err.Error())
-				w.Write([]byte(err.Error()))
-				return
-			}
-			w.WriteHeader(post.StatusCode)
-			w.Write(r)
+			response, err = request.Post(uri)
 		}
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		res, err := gzip.NewReader(response.Body)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.Write([]byte(err.Error()))
+			return
+		}
+		responseBody, err := io.ReadAll(res)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(response.StatusCode)
+		w.Write(responseBody)
 	})
 
 	err := http.ListenAndServe("0.0.0.0:80", r)
